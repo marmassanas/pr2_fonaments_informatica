9e7989cd84e9a2ee1a0126eae00f6812a149dd42
marmassanas <mar.massanas@autonoma.cat>
2024-10-21 16:51:50 +0200

    exercicis

--- pr2_FI.c
+++ pr2_FI.c
@@ -33,5 +33,143 @@ void InitData() {
     }
 }
 
+void PrintVect( float vect[N], int from, int numel ){
+// Validem que 'from + numel' no excedeixi la mida del vector
+    if (from + numel > N) {
+        printf("Error: El rang excedeix la mida del vector.\n");
+        return;
+    }
 
+    // Imprimim els elements del vector
+    for (int i = from; i < from + numel; i++) {
+        printf("%f ", vect[i]);
+    }
+}
 
+void PrintRow( float mat[N][N], int row, int from, int numel ){
+    if (from + numel > N) {
+        printf("Error: El rang excedeix la mida del vector.\n");
+        return;
+    }
+
+    for(int i=from;i<from+numel;i++){
+        print("%f ", mat[row][i]);
+    }
+}
+
+void MultEscalar( float vect[N], float vectres[N], float alfa ){
+    for (int i=0;i<N;i++){
+        vectres[i]=vect[i]*alfa;
+    }
+}
+
+float Scalar( float vect1[N], float vect2[N] ){
+    float scalar=0.0;
+    for (int i=0;i<N;i++){
+        scalar+=(vect1[i]*vect2[i]);
+    }
+    return scalar;
+}
+
+float Magnitude( float vect[N] ){
+    return sqrt(Scalar(vect, vect));
+}
+
+int Ortogonal( float vect1[N], float vect2[N] ){
+    for(int i=0; i<N;i++){
+        if(Scalar(vect1, vect2)==0){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
+
+void Projection( float vect1[N], float vect2[N], float vectres[N] ){
+    float numerador=Scalar(vect1, vect2);
+    float denominador=Magnitude(vect2);
+    for (int i=0;i<N;i++){
+        vectres[i]=(numerador/denominador)*vect2[i];
+    }
+}
+
+float Infininorm(float M[N][N]) {
+    float maxSum = 0.0;  
+    for (int i = 0; i < N; i++) {
+        float rowSum = 0.0;  // Suma dels valors absoluts de la fila
+        for (int j = 0; j < N; j++) {
+            rowSum += fabs(M[i][j]);
+        }
+        if (rowSum > maxSum) {
+            maxSum = rowSum;
+        }
+    }
+    return maxSum;  // Retorna la infini-norma
+}
+
+float Onenorm( float M[N][N] ){
+    float maxSum = 0.0;  
+    for (int i = 0; i < N; i++) { 
+        float colSum = 0.0; 
+        for (int j = 0; j < N; j++) {
+            colSum += fabs(M[j][i]);
+        }
+        if (colSum > maxSum) {
+            maxSum = colSum;
+        }
+    }
+    return maxSum;  
+}
+
+float NormFrobenius( float M[N][N] ){
+    float sum=0.0;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            sum+=M[i][j]*M[i][j];
+        }
+    }
+    return sqrt(sum);
+}
+
+int DiagonalDom( float M[N][N] ){
+    float sumDiagonal=0.0;
+    float sumValors=0.0;
+
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            if(i==j){
+                sumDiagonal+=M[i][j];
+            }
+            else{
+                sumValors+=fabs(M[i][j]);
+            }
+        }
+    }
+    if(fabs(sumDiagonal)>sumValors){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void Matriu_x_Vector( float M[N][N], float vect[N], float vectres[N] ){
+    for (int i = 0; i < N; i++) {
+        vectres[i] = 0;  // Inicialitzar a 0 abans de sumar
+    }
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            vectres[i]+=M[j][i]*vect[i];
+        }
+    }
+}
+
+int Jacobi( float M[N][N] , float vect[N], float vectres[N], unsigned iter ){
+
+}
+
+int main(){
+    InitData();  // Inicialitzar dades
+
+}
