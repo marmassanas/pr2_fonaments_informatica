7474dc0cd783f5bcd920bbbc8f5cd85ea068fa6e
marmassanas <mar.massanas@autonoma.cat>
2024-10-21 14:47:52 +0200

    Modificat pr2_FI.c amb noves funcionalitats

--- pr2_FI.c
+++ pr2_FI.c
@@ -1,3 +1,37 @@
-#!#bin/bash
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+#define N 512
+
+// Matrius i vectors
+float Mat[N][N];        // Matriu de N x N
+float MatDD[N][N];     // Matriu diagonal dominant
+float V1[N];           // Vector 1
+float V2[N];           // Vector 2
+float V3[N];           // Vector 3
+float V4[N];           // Vector 4
+
+void InitData() {
+    int i, j;
+    srand(334411);  // Inicialització de la llavor
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++) {
+            Mat[i][j] = (((i * j) % 3) ? -1 : 1) * (100.0 * (rand() / (1.0 * RAND_MAX)));
+            if ((abs(i - j) <= 3) && (i != j))
+                MatDD[i][j] = (((i * j) % 3) ? -1 : 1) * (rand() / (1.0 * RAND_MAX));
+            else if (i == j)
+                MatDD[i][j] = (((i * j) % 3) ? -1 : 1) * (10000.0 * (rand() / (1.0 * RAND_MAX)));
+            else 
+                MatDD[i][j] = 0.0;
+        }
+    for (i = 0; i < N; i++) {
+        V1[i] = (i < N / 2) ? (((i * j) % 3) ? -1 : 1) * (100.0 * (rand() / (1.0 * RAND_MAX))) : 0.0;
+        V2[i] = (i >= N / 2) ? (((i * j) % 3) ? -1 : 1) * (100.0 * (rand() / (1.0 * RAND_MAX))) : 0.0;
+        V3[i] = (((i * j) % 5) ? -1 : 1) * (100.0 * (rand() / (1.0 * RAND_MAX)));
+    }
+}
+
 
 
